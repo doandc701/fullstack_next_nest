63e9a621e5227218da13db6d22d77d8c15bbcf8b
Nguyen Xuan Doan <nguyenxuandoan701scsoft@gmail.com>
2024-09-10 22:58:30 +0700

    Init pr

---
+++ admin-web/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from 'antd';
+export default function Home() {
+  return (
+    <div>Hello World
+      <div>
+        <Button type='primary'>Click Antd</Button>
+      </div>
+    </div>
+  );
+}
